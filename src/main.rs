d1bf22fa6f5027965706bd9ff834a9dba4c2f51f
Ashish Binu <ashishbinu90@gmail.com>
2023-07-15 22:13:21 +0530

    feat: add debug details to debug.log

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use flexi_logger::FileSpec;
 
 fn main() -> Result<()> {
     flexi_logger::Logger::try_with_str("debug")?
-        .log_to_file(FileSpec::default())
+        .log_to_file(FileSpec::try_from("./debug.log")?)
         .start()?;
     Editor::default()?.run()
 }
